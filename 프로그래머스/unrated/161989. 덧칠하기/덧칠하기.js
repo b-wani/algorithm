985ec996160b7e5dc6a64f58b4573dbeec97b120
Wani <devwani93@gmail.com>
2023-05-10 09:56:16 +0900

    [unrated] Title: 덧칠하기, Time: 2.49 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,23 +1,13 @@
 function solution(n, m, section) {
-    // 처음 색칠할 벽의 위치를 구한다
-    // 롤러 길이 만큼 더한다
-    // 
-    if(m === 1) {
-        return section.length;
-    }
+    let cnt = 0;
+    let rollerPos = 0;
     
-    let cnt = 1;
-    let rollerPos = section[0] + m;
-    
-    for(let i = 1; i < section.length; i++) {
-        if(rollerPos === section[i]) {
-            rollerPos += m;
-            cnt++;
-        } else if(rollerPos < section[i]) {
-            rollerPos = section[i] + m;
+    section.forEach((wall) => {
+        if(rollerPos < wall) {
+            rollerPos = wall + m - 1;
             cnt++;
         }
-    }
+    })
     
     return cnt;
 }
